d51ab476294bfb8d8307b4b926a65169e838a2a2
jonathanfds <jonathan.fd.silva@gmail.com>
2015-04-29 15:32:20 -0300

    Close frmComparar

--- AproveitamentoDeNotas/frmComparar.Designer.cs
+++ AproveitamentoDeNotas/frmComparar.Designer.cs
@@ -29,43 +29,34 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmComparar));
-            this.cmbCurso = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.cmbInstituicao = new System.Windows.Forms.ComboBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.txtCargaHoraria = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtNomeMateria = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label10 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // cmbCurso
-            // 
-            this.cmbCurso.FormattingEnabled = true;
-            this.cmbCurso.Location = new System.Drawing.Point(140, 115);
-            this.cmbCurso.Name = "cmbCurso";
-            this.cmbCurso.Size = new System.Drawing.Size(318, 25);
-            this.cmbCurso.TabIndex = 34;
-            this.cmbCurso.Text = "Análise e desenvolvimento de sistemas";
             // 
             // label5
             // 
@@ -77,34 +68,16 @@
             this.label5.TabIndex = 33;
             this.label5.Text = "Curso:";
             // 
-            // cmbInstituicao
+            // btnCancel
             // 
-            this.cmbInstituicao.FormattingEnabled = true;
-            this.cmbInstituicao.Location = new System.Drawing.Point(140, 72);
-            this.cmbInstituicao.Name = "cmbInstituicao";
-            this.cmbInstituicao.Size = new System.Drawing.Size(411, 25);
-            this.cmbInstituicao.TabIndex = 32;
-            this.cmbInstituicao.Text = "FATEC - Carapicuíba";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(666, 424);
-            this.button2.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 35);
-            this.button2.TabIndex = 31;
-            this.button2.Text = "Cancelar";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(140, 203);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(413, 182);
-            this.textBox4.TabIndex = 29;
-            this.textBox4.Text = resources.GetString("textBox4.Text");
+            this.btnCancel.Location = new System.Drawing.Point(604, 424);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(94, 35);
+            this.btnCancel.TabIndex = 31;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // label4
             // 
@@ -115,15 +88,6 @@
             this.label4.Size = new System.Drawing.Size(70, 19);
             this.label4.TabIndex = 28;
             this.label4.Text = "Descrição:";
-            // 
-            // txtCargaHoraria
-            // 
-            this.txtCargaHoraria.Location = new System.Drawing.Point(140, 160);
-            this.txtCargaHoraria.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
-            this.txtCargaHoraria.Name = "txtCargaHoraria";
-            this.txtCargaHoraria.Size = new System.Drawing.Size(63, 25);
-            this.txtCargaHoraria.TabIndex = 27;
-            this.txtCargaHoraria.Text = "80";
             // 
             // label3
             // 
@@ -145,15 +109,6 @@
             this.label2.TabIndex = 25;
             this.label2.Text = "Instituição:";
             // 
-            // txtNomeMateria
-            // 
-            this.txtNomeMateria.Location = new System.Drawing.Point(140, 30);
-            this.txtNomeMateria.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
-            this.txtNomeMateria.Name = "txtNomeMateria";
-            this.txtNomeMateria.Size = new System.Drawing.Size(411, 25);
-            this.txtNomeMateria.TabIndex = 24;
-            this.txtNomeMateria.Text = "PROGRAMAÇÃO EM MICROINFORMÁTICA";
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -166,60 +121,41 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.cmbCurso);
+            this.groupBox1.Controls.Add(this.label15);
+            this.groupBox1.Controls.Add(this.label14);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label11);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.txtNomeMateria);
-            this.groupBox1.Controls.Add(this.cmbInstituicao);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.txtCargaHoraria);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(564, 406);
+            this.groupBox1.Size = new System.Drawing.Size(528, 406);
             this.groupBox1.TabIndex = 35;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Matéria Requerida";
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.textBox1);
-            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.label16);
+            this.groupBox2.Controls.Add(this.label17);
+            this.groupBox2.Controls.Add(this.label18);
+            this.groupBox2.Controls.Add(this.label19);
+            this.groupBox2.Controls.Add(this.label20);
             this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Controls.Add(this.label7);
-            this.groupBox2.Controls.Add(this.textBox2);
-            this.groupBox2.Controls.Add(this.comboBox2);
             this.groupBox2.Controls.Add(this.label8);
             this.groupBox2.Controls.Add(this.label9);
-            this.groupBox2.Controls.Add(this.textBox3);
             this.groupBox2.Controls.Add(this.label10);
-            this.groupBox2.Location = new System.Drawing.Point(607, 12);
+            this.groupBox2.Location = new System.Drawing.Point(549, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(564, 406);
+            this.groupBox2.Size = new System.Drawing.Size(524, 406);
             this.groupBox2.TabIndex = 36;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Matéria Apresentada";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(140, 203);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(413, 182);
-            this.textBox1.TabIndex = 29;
-            this.textBox1.Text = resources.GetString("textBox1.Text");
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(140, 115);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(318, 25);
-            this.comboBox1.TabIndex = 34;
-            this.comboBox1.Text = "Análise e desenvolvimento de sistemas";
             // 
             // label6
             // 
@@ -241,24 +177,6 @@
             this.label7.TabIndex = 33;
             this.label7.Text = "Curso:";
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(140, 30);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(411, 25);
-            this.textBox2.TabIndex = 24;
-            this.textBox2.Text = "PROGRAMAÇÃO EM MICROINFORMÁTICA";
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(140, 72);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(411, 25);
-            this.comboBox2.TabIndex = 32;
-            this.comboBox2.Text = "FATEC - São Paulo";
-            // 
             // label8
             // 
             this.label8.AutoSize = true;
@@ -279,15 +197,6 @@
             this.label9.TabIndex = 26;
             this.label9.Text = "Carga Horária:";
             // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(140, 160);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(63, 25);
-            this.textBox3.TabIndex = 27;
-            this.textBox3.Text = "80";
-            // 
             // label10
             // 
             this.label10.AutoSize = true;
@@ -300,7 +209,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(562, 424);
+            this.button1.Location = new System.Drawing.Point(500, 424);
             this.button1.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 35);
@@ -310,7 +219,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(458, 424);
+            this.button3.Location = new System.Drawing.Point(397, 424);
             this.button3.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(94, 35);
@@ -318,16 +227,104 @@
             this.button3.Text = "Aprovar";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(120, 33);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(278, 19);
+            this.label11.TabIndex = 34;
+            this.label11.Text = "PROGRAMAÇÃO EM MICROINFORMÁTICA";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(120, 75);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(133, 19);
+            this.label12.TabIndex = 35;
+            this.label12.Text = "FATEC - Carapicuíba";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(120, 118);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(246, 19);
+            this.label13.TabIndex = 36;
+            this.label13.Text = "Análise e desenvolvimento de sistemas";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(120, 163);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(25, 19);
+            this.label14.TabIndex = 37;
+            this.label14.Text = "80";
+            // 
+            // label15
+            // 
+            this.label15.Location = new System.Drawing.Point(120, 203);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(404, 188);
+            this.label15.TabIndex = 38;
+            this.label15.Text = resources.GetString("label15.Text");
+            // 
+            // label16
+            // 
+            this.label16.Location = new System.Drawing.Point(117, 203);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(404, 188);
+            this.label16.TabIndex = 43;
+            this.label16.Text = resources.GetString("label16.Text");
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(117, 163);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(25, 19);
+            this.label17.TabIndex = 42;
+            this.label17.Text = "80";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(117, 118);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(246, 19);
+            this.label18.TabIndex = 41;
+            this.label18.Text = "Análise e desenvolvimento de sistemas";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(117, 75);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(133, 19);
+            this.label19.TabIndex = 40;
+            this.label19.Text = "FATEC - Carapicuíba";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(117, 33);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(278, 19);
+            this.label20.TabIndex = 39;
+            this.label20.Text = "PROGRAMAÇÃO EM MICROINFORMÁTICA";
+            // 
             // frmComparar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1177, 468);
+            this.ClientSize = new System.Drawing.Size(1083, 468);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnCancel);
             this.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.Name = "frmComparar";
             this.Text = "Comparar Matérias";
@@ -341,30 +338,30 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox cmbCurso;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ComboBox cmbInstituicao;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtCargaHoraria;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtNomeMateria;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
     }
 }
